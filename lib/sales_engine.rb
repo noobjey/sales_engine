e15e6f67f58d66bbbeb57d0141cf7000e0123368
Adam Caron <adamwcaron@gmail.com>
2015-06-16 08:42:41 -0600

    sales creates a merchant repository

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,3 +1,12 @@
-class SalesEngine
+require 'csv'
+require './lib/merchant_repository'
 
+class SalesEngine
+  def startup
+
+  end
+
+  def merchant_repository
+    MerchantRepository.new
+  end
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -2,7 +2,12 @@ require './test/test_helper'
 require './lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
-  def test_it
-    assert !!SalesEngine.new
+
+  def test_it_has_a_merchant_repository
+    engine = SalesEngine.new
+
+    engine.startup
+
+    assert engine.merchant_repository.is_a?(MerchantRepository), "is a: #{engine.merchant_repository.class}"
   end
 end
