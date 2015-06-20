900e52c8fbef043f0b74469a486c2640a0ab1e5e
Jason Wright <noobjey@gmail.com>
2015-06-20 16:04:58 -0600

    merchant repo can find a merchant

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -2,6 +2,25 @@ require_relative 'test_helper'
 require_relative '../lib/merchant_repository'
 
 class MerchantRepositoryTest < Minitest::Test
+  attr_reader :merchants
+
+  class FakeMerchant
+    attr_reader :id
+
+    def initialize(id)
+      @id = id
+    end
+  end
+
+  def setup
+    merchant1 = FakeMerchant.new(1)
+    merchant2 = FakeMerchant.new(2)
+    merchant3 = FakeMerchant.new(3)
+
+    @merchants = [merchant1, merchant2, merchant3]
+
+  end
+
   def test_it_knows_its_parent
     sales_engine = "fake_sales_engine"
 
@@ -19,9 +38,9 @@ class MerchantRepositoryTest < Minitest::Test
   end
 
   def test_it_loads_the_data
-    path = './data/fixtures/merchants.csv'
+    path         = './data/fixtures/merchants.csv'
     sales_engine = "fake_sales_engine"
-    repo = MerchantRepository.new(sales_engine)
+    repo         = MerchantRepository.new(sales_engine)
 
     repo.load_data(path)
 
@@ -31,11 +50,22 @@ class MerchantRepositoryTest < Minitest::Test
 
   def test_it_finds_items_by_merchant_id
     sales_engine = Minitest::Mock.new
-    repo = MerchantRepository.new(sales_engine)
+    repo         = MerchantRepository.new(sales_engine)
 
     sales_engine.expect(:find_items_by_merchant_id, nil, [1])
     repo.find_items(1)
 
     sales_engine.verify
+  end
+
+  def test_it_finds_merchant_by_id
+    sales_engine   = "fake sales engine"
+    repo           = MerchantRepository.new(sales_engine)
+    id             = 1
+    repo.merchants = merchants
+
+    merchant = repo.find_merchant(id)
+
+    assert_equal id, merchant.id
   end
 end
