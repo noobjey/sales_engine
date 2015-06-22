a9e5f0784a9ccd3748e3243db4ee8bbce4fb0ce4
Jason Wright <noobjey@gmail.com>
2015-06-22 10:42:42 -0600

    added some comments to know if repo was going upchain or downchain

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -19,15 +19,17 @@ class ItemRepository
     end
   end
 
+  #Going down the chain
   def find_merchant_by_id(id)
     sales_engine.find_merchant_by_id(1)
   end
 
-  def find_items_by_merchant_id(id)
-    items.select { |item| item.merchant_id.eql?(id) }
-  end
-
+  # Going up the chain
   def find_invoice_items_by_id(id)
     sales_engine.find_invoice_items_by_item_id(id)
+  end
+
+  def find_items_by_merchant_id(id)
+    items.select { |item| item.merchant_id.eql?(id) }
   end
 end
