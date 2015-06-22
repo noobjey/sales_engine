353b0a5968a6ceb243883833fc33bcaea7e91e36
Adam Caron <adamwcaron@gmail.com>
2015-06-22 12:01:28 -0600

    creates invoice_repo and passes loads invoice data

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,13 +1,14 @@
 require_relative 'merchant_repository'
 require_relative 'item_repository'
+require_relative 'invoice_repository'
 # # require_relative 'customer_repository'
 # # require_relative 'transaction_repository'
 
 class SalesEngine
   attr_accessor :merchant_repository,
-                :item_repository #,
+                :item_repository,
+                :invoice_repository#,
 #               # :customer_repository,
-#               # :invoice_repository,
 #               # :invoice_item_repository,
 #               # :transaction_repository,
 
@@ -22,12 +23,12 @@ class SalesEngine
     @merchant_repository.load_data("#{filepath}/merchants.csv")
     @item_repository ||= ItemRepository.new(self)
     @item_repository.load_data("#{filepath}/items.csv")
+    @invoice_repository ||= InvoiceRepository.new(self)
+    @invoice_repository.load_data("#{@filepath}/invoices.csv")
     # @customer_repository ||= CustomerRepository.new(self)
     # @customer_repository.load_data("#{@filepath}/customers.csv")
     # @transaction_repository ||= TransactionRepository.new(self)
     # @transaction_repository.load_data("#{@filepath}/transactions.csv")
-    # @invoice_repository ||= InvoiceRepository.new(self)
-    # @invoice_repository.load_data("#{@filepath}/invoices.csv")
     # @invoice_item_repository ||= InvoiceItemRepository.new(self)
     # @invoice_item_repository.load_data("#{@filepath}/invoice_items.csv")
   end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -12,14 +12,6 @@ class SalesEngineTest < Minitest::Test
     assert_equal true, engine.merchant_repository.is_a?(MerchantRepository)
   end
 
-  def test_it_passes_itself_to_merchant_repository
-    engine = SalesEngine.new("./data/fixtures")
-
-    engine.startup
-
-    assert_equal engine, engine.merchant_repository.sales_engine
-  end
-
   def test_it_loads_the_merchant_data
     path   = "./data/fixtures"
     engine = SalesEngine.new(path)
@@ -45,14 +37,6 @@ class SalesEngineTest < Minitest::Test
     engine.startup
 
     assert_equal true, engine.item_repository.is_a?(ItemRepository)
-  end
-
-  def test_it_passes_itself_to_item_repository
-    engine = SalesEngine.new("./data/fixtures")
-
-    engine.startup
-
-    assert_equal engine, engine.item_repository.sales_engine
   end
 
   def test_it_loads_the_items_data
@@ -89,13 +73,53 @@ class SalesEngineTest < Minitest::Test
     repo.verify
   end
 
+  def test_it_creates_an_ivoice_repository
+    engine = SalesEngine.new("./data/fixtures")
+
+    engine.startup
+
+    assert_equal true, engine.invoice_repository.is_a?(InvoiceRepository)
+  end
+
+  def test_it_loads_the_invoice_data
+    path   = "./data/fixtures"
+    engine = SalesEngine.new(path)
+    repo   = Minitest::Mock.new
+
+    engine.invoice_repository = repo
+    repo.expect(:load_data, nil, ["#{path}/invoices.csv"])
+    engine.startup
+
+    repo.verify
+  end
+
+  def test_it_passes_itself_to_repositories
+    engine = SalesEngine.new("./data/fixtures")
+
+    engine.startup
+
+    assert_equal engine, engine.item_repository.sales_engine
+    assert_equal engine, engine.item_repository.sales_engine
+    assert_equal engine, engine.merchant_repository.sales_engine
+  end
+
   def test_smoke
+    skip
     engine = SalesEngine.new("./data/fixtures")
 
     engine.startup
 
     assert_equal 'Schroeder-Jerde', engine.find_merchant_by_id(1).name
-    assert_equal 10, engine.find_items_by_merchant_id(1).length
+    assert_equal 10,    engine.find_items_by_merchant_id(1).length
     assert_equal 32301, engine.find_items_by_merchant_id(1)[2].unit_price
+
+    assert_equal 33,    engine.find_by_invoice_id(4).merchant_id
+    assert_equal 1,     engine.find_invoices_by_merchant_id(78).length
+    assert_equal 3,     engine.find_invoices_by_merchant_id(78)[2].invoice_id
+    #  id, customer_id, merchant_id, status, created_at, updated_at
+    #   1, 1, 26, shipped,2012-03-25 09:54:09 UTC,2012-03-25 09:54:09 UTC
+    #   2, 1, 75, shipped,2012-03-12 05:54:09 UTC,2012-03-12 05:54:09 UTC
+    #   3, 1, 78, shipped,2012-03-10 00:54:09 UTC,2012-03-10 00:54:09 UTC
+    #   4, 1, 33, shipped,2012-03-24 15:54:10 UTC,2012-03-24 15:54:10 UTC
   end
 end
