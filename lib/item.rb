aff2e817b16f0eb4cd387ae07dcbcf0f695b58ed
Jason Wright <noobjey@gmail.com>
2015-06-22 19:40:06 -0600

    added big decimal to item for unit price

--- lib/item.rb
+++ lib/item.rb
@@ -1,13 +1,14 @@
 require_relative 'merchant'
+require 'bigdecimal'
 
 class Item
-  attr_reader   :id,
-                :name,
-                :description,
-                :unit_price,
-                :merchant_id,
-                :created_at,
-                :updated_at
+  attr_reader :id,
+              :name,
+              :description,
+              :unit_price,
+              :merchant_id,
+              :created_at,
+              :updated_at
 
   attr_accessor :repository
 
@@ -15,7 +16,7 @@ class Item
     @id          = line[:id].to_i
     @name        = line[:name]
     @description = line[:description]
-    @unit_price  = line[:unit_price].to_i
+    @unit_price  = BigDecimal.new(line[:unit_price])
     @merchant_id = line[:merchant_id].to_i
     @created_at  = line[:created_at]
     @updated_at  = line[:updated_at]
