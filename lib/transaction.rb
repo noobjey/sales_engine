6febe751a3b9ebcf3f1c8800eb85c2069faa232a
Jason Wright <noobjey@gmail.com>
2015-06-23 15:59:43 -0600

    transaction object created

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -5,24 +5,18 @@ class Transaction
               :credit_card_expiration_date,
               :result,
               :created_at,
-              :updated_at
+              :updated_at,
+              :repository
 
-  def initialize(
-    id,
-    invoice_id,
-    credit_card_number,
-    credit_card_expiration_date,
-    result,
-    created_at,
-    updated_at
-  )
+  def initialize(line, repository)
 
-    @id = id
-    @invoice_id = invoice_id
-    @credit_card_number = credit_card_number
-    @credit_card_expiration_date = credit_card_expiration_date
-    @result = result
-    @created_at = created_at
-    @updated_at = updated_at
+    @id                          = line[:id].to_i
+    @invoice_id                  = line[:invoice_id].to_i
+    @credit_card_number          = line[:credit_card_number]
+    @credit_card_expiration_date = line[:credit_card_expiration_date]
+    @result                      = line[:result]
+    @created_at                  = line[:created_at]
+    @updated_at                  = line[:updated_at]
+    @repository                  = repository
   end
 end
