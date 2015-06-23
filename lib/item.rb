3b2ec4fe574a3318779c18bc0497131b0264c857
Jason Wright <noobjey@gmail.com>
2015-06-23 15:23:35 -0600

    updated item to use big decimal

--- lib/item.rb
+++ lib/item.rb
@@ -16,7 +16,7 @@ class Item
     @id          = line[:id].to_i
     @name        = line[:name]
     @description = line[:description]
-    @unit_price  = BigDecimal.new(line[:unit_price])
+    @unit_price  = BigDecimal.new(line[:unit_price])/100
     @merchant_id = line[:merchant_id].to_i
     @created_at  = line[:created_at]
     @updated_at  = line[:updated_at]
