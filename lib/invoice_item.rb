bda872f202c5b322d7f3a2119cb36aabd71944b4
Adam Caron <adamwcaron@gmail.com>
2015-06-23 18:41:59 -0600

    changes unit_price to BigDecimal format

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,3 +1,5 @@
+# require 'bigdecimal'
+
 class InvoiceItem
   attr_reader :id,
               :item_id,
@@ -12,7 +14,7 @@ class InvoiceItem
     @item_id    = line[:item_id].to_i
     @invoice_id = line[:invoice_id].to_i
     @quantity   = line[:quantity].to_i
-    @unit_price = line[:unit_price].to_i
+    @unit_price = BigDecimal.new(line[:unit_price])/100
     @created_at = line[:created_at]
     @updated_at = line[:updated_at]
     @repository = repository
