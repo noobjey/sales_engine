c8e06e21ad09c94e18d5539a2720766ffd5cf0b1
Adam Caron <adamwcaron@gmail.com>
2015-06-23 21:05:24 -0600

    sets up invoice test data

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -2,6 +2,31 @@ require_relative 'test_helper'
 require_relative '../lib/invoice_repository'
 
 class InvoiceRepositoryTest < Minitest::Test
+  attr_reader :invoices,
+              :fake_sales_engine,
+              :fixture_path,
+              :invoice_input
+
+  def setup
+    @fake_sales_engine = "fake sales engine"
+    @fixture_path      = './data/fixtures/invoices.csv'
+    @invoice_input = {
+      id:           1,
+      customer_id:  1,
+      merchant_id: 26,
+      status:      "shipped",
+      created_at:  "2012-03-25 09:54:09 UTC",
+      updated_at:  "2012-03-25 09:54:09 UTC"
+    }
+
+    invoice1 = Invoice.new(invoice_input, nil)
+    invoice2 = Invoice.new(invoice_input, nil)
+    invoice3 = Invoice.new(invoice_input, nil)
+    invoice4 = Invoice.new(invoice_input, nil)
+
+    @invoices = [invoice1, invoice2, invoice3, invoice4]
+  end
+
   def test_it_knows_its_parent
     sales_engine = "fake_sales_engine"
 
