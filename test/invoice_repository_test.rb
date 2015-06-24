a6bd1d7ff606e676f5b92f6a9dcf4402194bd520
Adam Caron <adamwcaron@gmail.com>
2015-06-23 21:13:08 -0600

    extracts file path

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -28,34 +28,30 @@ class InvoiceRepositoryTest < Minitest::Test
   end
 
   def test_it_knows_its_parent
-
     repo = InvoiceRepository.new(fake_sales_engine)
 
     assert fake_sales_engine, repo.sales_engine
   end
 
   def test_it_has_invoices
-
     repo = InvoiceRepository.new(fake_sales_engine)
 
     assert [], repo.invoices
   end
 
   def test_it_loads_the_data
-    path         = './data/fixtures/invoices.csv'
-    repo         = InvoiceRepository.new(fake_sales_engine)
+    repo = InvoiceRepository.new(fake_sales_engine)
 
-    repo.load_data(path)
+    repo.load_data(fixture_path)
 
     assert_equal 9, repo.invoices.size
     assert_equal 6, repo.invoices[5].id
   end
 
   def test_it_passes_itself_to_invoices
-    path         = './data/fixtures/invoices.csv'
-    repo         = InvoiceRepository.new(fake_sales_engine)
+    repo = InvoiceRepository.new(fake_sales_engine)
 
-    repo.load_data(path)
+    repo.load_data(fixture_path)
 
     assert_equal repo, repo.invoices.first.repository
   end
