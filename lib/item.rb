a492f8c29ca3c7b475ad71298923174f94c8de0d
Jason Wright <noobjey@gmail.com>
2015-06-25 07:46:48 -0600

    item repo most revenue

--- lib/item.rb
+++ lib/item.rb
@@ -31,16 +31,17 @@ class Item
     repository.find_invoice_items_by_id(id)
   end
 
-  # best_day returns the date with the most sales for the given item using the invoice date
   def best_day
     quantity_per_day = invoice_items.inject(Hash.new(0)) { |h, invoice_item| h[invoice_item.invoice.created_at.strftime("%F")] += invoice_item.quantity; h }
     best_day = quantity_per_day.sort_by { |k, v| v }.reverse.first.first
     Date.parse(best_day)
   end
 
-  private
-
   def quantity_sold
     invoice_items.inject(0) { |total, invoice_item| total + invoice_item.quantity }
+  end
+
+  def revenue
+    invoice_items.inject(0) { |total, invoice_item| total + (invoice_item.unit_price * invoice_item.quantity) }
   end
 end
