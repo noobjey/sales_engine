5c9c56de38b607a29bc031d45adc56a6ac3b0ddf
Adam Caron <adamwcaron@gmail.com>
2015-06-28 15:11:26 -0600

    returns the customer who has purchased the most items by quantity

--- lib/customer.rb
+++ lib/customer.rb
@@ -6,6 +6,8 @@ class Customer
               :updated_at,
               :repository
 
+  attr_accessor :items_purchased
+
   def initialize(line, repository)
     @id         = line[:id].to_i
     @first_name = line[:first_name]
@@ -13,6 +15,7 @@ class Customer
     @created_at = line[:created_at]
     @updated_at = line[:updated_at]
     @repository = repository
+    @items_purchased
   end
 
   def invoices
@@ -27,6 +30,20 @@ class Customer
     repository.find_merchant_by_merchant_id(favorite_merchant_id)
   end
 
+  def successful_invoices(invoices)
+    find_successful_transactions(invoices)
+  end
+
+  def total_items_purchased
+    successful_invoices = self.successful_invoices(self.invoices)
+    successful_inv_items = successful_invoices.map do |invoice|
+      invoice.invoice_items
+    end.flatten
+    self.items_purchased = successful_inv_items.inject(0) do |total, invoice_item|
+      total + (invoice_item.quantity)
+    end
+  end
+
   private
 
   def transactions_per_merchant
@@ -37,5 +54,15 @@ class Customer
 
   def favorite_merchant_id
     transactions_per_merchant.sort_by { |k, v| v }.reverse.first.first
+  end
+
+  def find_successful_transactions(invoices)
+    invoices.find_all { |invoice| has_successful_transactions?(invoice) }
+  end
+
+  def has_successful_transactions?(invoice)
+    invoice.transactions.any? do |transaction|
+      transaction.result.eql?('success')
+    end
   end
 end
